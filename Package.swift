aea061e70233cec51d0100aa11ff72894525da02
Audrey Sobgou Zebaze <audrey.sobgouzebaze@gmail.com>
2022-08-19 08:23:42 +0200

    Add package in Swift with xcframework not in Zip file

--- Package.swift
+++ Package.swift
@@ -10,15 +10,22 @@ let package = Package(
             name: "JoinStoriesSDK",
             targets: ["JoinStoriesSDK"]),
     ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        .package(url: "https://github.com/SnapKit/SnapKit.git", .upToNextMajor(from: "5.0.1")),
+        .package(url: "https://github.com/Alamofire/AlamofireImage.git", .upToNextMajor(from: "4.2.0")),
+        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "4.0.0")
+    ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
 
         .binaryTarget(
-            name: "JoinStoriesSDK",
-            url: "https://github.com/teamjoin/join-stories-sdk-ios-binary/blob/spm/Releases/1.0.2/JoinStoriesSDK-1.0.2.zip",
-            checksum: "9df6db7692147097381bf70776c1d7b0c02cef1e343515de0f15059fffdb48cc"
-        )
+             name: "JoinStoriesSDK",
+             path: "Releases/1.0.2/JoinStoriesSDK.xcframework"
+            // url: "https://github.com/mvpaudrey/JoinStories-Binaries/raw/main/JoinStories/JoinStories-0.1.6.zip",
+            // checksum: "66966a7519ee222c42090a6785a5730169f2a0d147251fd3344f6d3f0b832a82"
+         ),
     ],
     swiftLanguageVersions: [.v5]
 )
