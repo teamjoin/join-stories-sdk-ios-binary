ce8e8e1ce4834fbbe72b35a8f995f6a2ec5581df
Audrey Sobgou Zebaze <audrey.sobgouzebaze@gmail.com>
2022-08-22 22:44:11 +0200

    Sub dependencies

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "JoinStoriesSDK",
-            targets: ["JoinStoriesSDK"]),
+            targets: ["JoinStoriesSDKWrapper"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,14 +19,20 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-
+        .target(
+            name: "JoinStoriesSDKWrapper",
+            dependencies: [
+                "JoinStoriesSDK",
+                .product(name: "SnapKit", package: "SnapKit"),
+                .product(name: "AlamofireImage", package: "AlamofireImage"),
+                .product(name: "SwiftyJSON", package: "SwiftyJSON"),
+            ],
+            path: "Sources"
+        ),
         .binaryTarget(
-             name: "JoinStoriesSDK",
-             path: "Releases/1.0.2/JoinStoriesSDK.xcframework"
-            // url: "https://github.com/mvpaudrey/JoinStories-Binaries/raw/main/JoinStories/JoinStories-0.1.6.zip",
-            // checksum: "66966a7519ee222c42090a6785a5730169f2a0d147251fd3344f6d3f0b832a82"
-         ),
+            name: "JoinStoriesSDK",
+            path: "JoinStoriesSDK.xcframework"
+        ),
     ],
     swiftLanguageVersions: [.v5]
 )
-
---
+++ Sources/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Audrey SOBGOU ZEBAZE on 22/08/2022.
+//
+
+import Foundation
