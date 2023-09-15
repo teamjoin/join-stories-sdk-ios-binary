5d59e5523865eb9e43761e1dbf0763fbae434395
Max Lignereux <maxime.lignereux@teamjoin.fr>
2023-09-15 17:37:28 +0200

    update version to 1.5.0

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "JoinStoriesSDK",
-            url: "https://github.com/teamjoin/join-stories-sdk-ios-binary/raw/master/Releases/1.4.3/JoinStoriesSDK.zip",
-            checksum: "5af61ba0f23efc7979bd842d0d9dd636eb4b47876d61c2bddcb738050b6a4143"
+            url: "https://github.com/teamjoin/join-stories-sdk-ios-binary/raw/master/Releases/1.5.0/JoinStoriesSDK.zip",
+            checksum: "9758e0b246c45f45e706de82205bd54b366c080c65091611b7431f20f8e0861c"
         ),
     ],
     swiftLanguageVersions: [.v5]
