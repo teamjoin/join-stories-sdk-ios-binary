59bbedbc2d404326e843fff90063b4954eb7b2b6
dcombe <combedimitri@gmail.com>
2024-02-22 18:12:26 +0100

    update to version 3.0.0

--- Package.swift
+++ Package.swift
@@ -8,15 +8,15 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "JoinStoriesSDK",
-            targets: ["JoinStoriesSDK"]),
+            targets: ["JOINStoriesSDK"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
-            name: "JoinStoriesSDK",
-            url: "https://github.com/teamjoin/join-stories-sdk-ios-binary/raw/master/Releases/1.8.0/JoinStoriesSDK.zip",
-            checksum: "b47d022b3df97f3d56c1e8f5c29a4e67e80dd54757fffffdbdef659957a539ec"
+            name: "JOINStoriesSDK",
+            url: "https://github.com/teamjoin/join-stories-sdk-ios-binary/raw/master/Releases/3.0.0/JOINStoriesSDK.zip",
+            checksum: "b7c47f6d65c80274ff2258cfbdff1481e1276aa42ac5469d12029f55219c32d4"
         ),
     ],
     swiftLanguageVersions: [.v5]
